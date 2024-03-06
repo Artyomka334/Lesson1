00b5b0b9fb8b55ac650b620465ce28dab376dfab
Artyomka334 <55884736+Artyomka334@users.noreply.github.com>
2024-03-06 20:44:52 +0300

    Add lesson1

---
+++ buttonclicker/src/main/java/com/mirea/kachalovaa/buttonclicker/MainActivity.java
@@ -0,0 +1,41 @@
+package com.mirea.kachalovaa.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private CheckBox checkBox;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewStudent = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        checkBox = findViewById(R.id.checkBox);
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № 11");
+                checkBox.setChecked(false);
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+    public void onMyButtonClick(View v) {
+        Toast.makeText(this, "Это не я сделал", Toast.LENGTH_SHORT).show();
+        checkBox.setChecked(!checkBox.isChecked());
+    }
+
+}
